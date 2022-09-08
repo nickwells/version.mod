88a2b3cfd272aea11baf6bc275e482d66ac9b480
Nick Wells <wells.nicholas@gmail.com>
2022-09-08 11:13:18 +0100

    refactor: minor changes to keep gofumpt quiet

--- version/doc.go
+++ version/doc.go
@@ -3,17 +3,17 @@ version holds various strings describing the current version of a
 program. The intention is that the values are set through the -ldflags
 argument to the go build and go install tools as follows:
 
-  PKG=github.com/nickwells/version.mod/version
-  go build -ldflags="-X '$PKG.tag=v1.2.3' ..."
+	PKG=github.com/nickwells/version.mod/version
+	go build -ldflags="-X '$PKG.tag=v1.2.3' ..."
 
 The values that can be set are:
 
-  tag
-  commit
-  author
-  date
-  buildUser
-  buildDate
+	tag
+	commit
+	author
+	date
+	buildUser
+	buildDate
 
 Each of these values is a string.
 
@@ -22,14 +22,14 @@ platform is as follows:
 
 first set up a shell variable to hold the date format to avoid retyping
 
-  DATEFMT="%Y/%m/%d %H:%M:%S"
+	DATEFMT="%Y/%m/%d %H:%M:%S"
 
-  tag=$(git --no-pager tag --contains=HEAD)
-  commit=$(git rev-parse HEAD)
-  author=$(git --no-pager log -1 --format='%an')
-  date=$(git --no-pager log -1 --date="format:$DATEFMT" --format='%cd')
-  buildUser=$(id --user --real --name)
-  buildDate=$(date "+$DATEFMT")
+	tag=$(git --no-pager tag --contains=HEAD)
+	commit=$(git rev-parse HEAD)
+	author=$(git --no-pager log -1 --format='%an')
+	date=$(git --no-pager log -1 --date="format:$DATEFMT" --format='%cd')
+	buildUser=$(id --user --real --name)
+	buildDate=$(date "+$DATEFMT")
 
 Add these values after the package name as shown above with the -ldflags
 parameter to go build or go install.
@@ -41,12 +41,12 @@ If you wish to build the program without the checks that the build values
 have been set then build the program with the build tag version_no_check set
 as follows:
 
-  go build -tags version_no_check
+	go build -tags version_no_check
 
 Without this build tag a program built without any version information having
 been set will panic at startup.
 
-Deprecated: this package superseded by the information in runtime/debug. It
+Deprecated: this package is superseded by the information in runtime/debug. It
 is error-prone, depending as it does on the need for complex build-time
 parameters to be set up. It is no longer needed as some of the features it
 offered are now provided automatically by the runtime through the
